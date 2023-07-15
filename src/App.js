848cb6187e0e563d9d2891683add845d61bd8e50
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-15 11:46:58 +0200

    ReactTransitionGropu part3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Transition } from "react-transition-group";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -24,16 +24,27 @@ class App extends Component {
 			<div className='App'>
 				<h1>React Animations</h1>
 				<button
+					className='Button'
 					onClick={() =>
 						this.setState(prevState => ({ showBlock: !prevState.showBlock }))
 					}>
 					Toggle
 				</button>
-        <br/>
-				{this.state.showBlock ? (
-					<div
-						style={{ backgroundColor: "red", width: 100, height: 100, margin:"auto" }}></div>
-				) : null}
+				<br />
+				<Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit>
+					{state => (
+						<div
+							style={{
+								backgroundColor: "red",
+								width: 100,
+								height: 100,
+								margin: "auto",
+								transition:"opacity 1s ease-out",
+								opacity: state === "exiting" ? 0 : 1,
+							}}></div>
+					)}
+				</Transition>
+
 				{this.state.modalIsOpen ? (
 					<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
 				) : null}
